fa491cb49813c1073eb95450a0b263e37802a8ca
Cyrille <archibaldschmitz@gmail.com>
2018-01-25 12:58:45 +0100

    123, 456

--- Projet03bis/Projet03bis/Projet03bis.cpp
+++ Projet03bis/Projet03bis/Projet03bis.cpp
@@ -1,21 +1,87 @@
 #include "stdafx.h"
 #include "CSetInt.h"
 
-#define TAILLESTANDARD 50
+#include <iostream>
+using namespace std;
+
+int fctMenu(CSetInt *tab1);
 
 int main()
 {
 	CSetInt tab1(TAILLESTANDARD);
-	int i;
 
-	cout << "Manipulation d'un tableau d'entiers." << endl;	
-	for (i = 0; i < 5 + 1; i++)
+	do
 	{
-		if (!tab1.fctAddInt(i+1))
+		//system("cls");
+		cout << endl << "\t ---MANIPULATION D'UN TABLEAU D'ENTIERS---" << endl << endl;
+		cout << "[1] Entrer un nouvel entier positif." << endl;
+		cout << "[2] Afficher le tableau." << endl;
+		cout << "[3] Lancer une insertion automatiquer pour tester la taille max du tableau." << endl;
+		cout << "[0] Sortir du programme." << endl;
+	} while (fctMenu(&tab1) != STOP);
+
+	return 0;
+}
+
+//
+int fctMenu(CSetInt *tab1)
+{
+	int dChoix;
+
+	cout << "Votre choix : ";
+	cin >> dChoix;
+	cout << endl;
+
+	switch (dChoix)
+	{
+	case 0:												// Fin du programme.
+		cout << "Fin du programme." << endl;
+		return STOP;
+	case 1:													// Insertion d'un entier dans le tableau.
+		cout << "Entier : ";
+		cin >> dChoix;
+
+		try
 		{
-			cout << "Fin du tableau atteinte" << endl;
+			tab1->fctAddInt(dChoix);						// Insertion d'un nouvel entier dans le tableau.
+		}													// Contrôle : espace disponible et éventuel  
+		catch (int dA)										// présence de l'entier.
+		{
+			if (dA == ERREURTAILLE)
+			{
+				cerr << "Erreur : taille du tableau maximale atteinte" << endl;
+			}
+			if (dA == ERREURINTEXISTS)
+			{
+				cerr << "Erreur : entier deja present au sein du tableau" << endl;
+			}
 		}
+		return CONTINUE;
+	case 2:													// Affichage du tableau.
+		tab1->fctShowTable();
+		return CONTINUE;
+	case 3:													// Insertion d'une chaîne d'entier dans le tableau
+		for (int i = 0; i < TAILLESTANDARD + 1; i++)		// jusqu'à atteindre la taille maximale.
+		{
+			try
+			{
+				tab1->fctAddInt(i + 1);
+			}
+			catch (int dA)										
+			{
+				if (dA == ERREURTAILLE)
+				{
+					cerr << "Erreur : taille du tableau maximale atteinte" << endl;
+				}
+				if (dA == ERREURINTEXISTS)
+				{
+					cerr << "Erreur : entier deja present au sein du tableau" << endl;
+				}
+			}
+		}
+		return CONTINUE;
+	default:
+		cout << "Choix non valide, fin du programme" << endl;// Fin du programme.
+		return STOP;
 	}
-	cout << tab1.fctShowTable() << endl;
-return 0;
 }
